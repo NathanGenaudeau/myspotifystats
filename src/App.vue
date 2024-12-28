62434a93341e900b7056359f712cb7109d7914ec
NathanGenaudeau <nathan.genaudeau@gmail.com>
2024-12-28 01:14:27 +0100

    add 2 stats (most listened-to hour and day), add dayjs and font

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import dayjs from 'dayjs'
+import weekday from 'dayjs/plugin/weekday'
+import duration from 'dayjs/plugin/duration'
+dayjs.extend(weekday);
+dayjs.extend(duration);
 
 interface Song {
   ts: string,
@@ -26,7 +31,9 @@ interface Song {
 
 const data = ref<any>();
 const dataParsed = ref<Song[]>([]);
-const list = ref<any>([]);
+
+const files = ref<File[]>([]);
+const tab = ref<string>();
 
 const readAsText = async (file: any) => {
 		return new Promise((resolve, _)=>{
@@ -38,18 +45,27 @@ const readAsText = async (file: any) => {
 		})
 }
 
-const files = ref<Blob[]>([]);
-
 const upload = async () => {
   data.value = await Promise.all(files.value.map(f => { return readAsText(f) }));
   dataParsed.value = data.value.flatMap((r : any) => JSON.parse(r.data)).filter((song: Song) => song.ms_played >= 20000);
 }
 
-const tab = ref<string>();
+const durationParsed = (time: number) => {
+  const days = Math.trunc(dayjs.duration({ seconds: time }).asDays());
+  const hours = Math.trunc(dayjs.duration({ seconds: (time - dayjs.duration({ days: days }).asSeconds()) }).asHours());
+  const minutes = Math.trunc(dayjs.duration({ seconds: (time - (dayjs.duration({ days: days }).asSeconds() + dayjs.duration({ hours: hours }).asSeconds())) }).asMinutes());
+  const seconds = Math.trunc(dayjs.duration({ seconds: (time - (dayjs.duration({ days: days }).asSeconds() + dayjs.duration({ hours: hours }).asSeconds() + dayjs.duration({ minutes: minutes }).asSeconds())) }).asSeconds());
+  
+  return `${days}d, ${hours}h, ${minutes}m, ${seconds}s`;
+}
+
+const mostListenedSongList = ref<any[]>([]);
+const mostListenedArtistList = ref<any[]>([]);
 
 const mostListenedSong = () => {
+  if (mostListenedSongList.value.length !== 0) return;
   // regroupement par musique
-  list.value = Object.values(dataParsed.value.reduce(
+  const list = Object.values(dataParsed.value.reduce(
     (r: any, c: Song) => {
       r[c.spotify_track_uri] = r[c.spotify_track_uri] || {id: c.spotify_track_uri, name: c.master_metadata_track_name, artist: c.master_metadata_album_artist_name, total_time: Math.trunc(c.ms_played/1000), count: 1};
       r[c.spotify_track_uri].total_time += Math.trunc(c.ms_played/1000);
@@ -57,12 +73,14 @@ const mostListenedSong = () => {
       return r;
     },{}));
 
-  list.value.sort((a: any, b: any) => b.count - a.count);
+  list.sort((a: any, b: any) => b.count - a.count);
+  mostListenedSongList.value = list;
 }
 
 const mostListenedArtist = () => {
+  if (mostListenedArtistList.value.length !== 0) return;
   // regroupement par artiste
-  list.value = Object.values(dataParsed.value.reduce(
+  const list = Object.values(dataParsed.value.reduce(
     (r: any, c: Song) => {
       r[c.master_metadata_album_artist_name] = r[c.master_metadata_album_artist_name] || {artist: c.master_metadata_album_artist_name, total_time: Math.trunc(c.ms_played/1000), count: 1};
       r[c.master_metadata_album_artist_name].total_time += Math.trunc(c.ms_played/1000);
@@ -70,12 +88,13 @@ const mostListenedArtist = () => {
       return r;
     },{}));
 
-  list.value.sort((a: any, b: any) => b.count - a.count);
+  list.sort((a: any, b: any) => b.count - a.count);
+  mostListenedArtistList.value = list;
 }
 
-const mostListenedAlbum = () => {
+const mostListenedAlbum = ():any[] => {
   // regroupement par album
-  list.value = Object.values(dataParsed.value.reduce(
+  const list = Object.values(dataParsed.value.reduce(
     (r: any, c: Song) => {
       r[c.master_metadata_album_album_name] = r[c.master_metadata_album_album_name] || {album: c.master_metadata_album_album_name, total_time: Math.trunc(c.ms_played/1000), count: 1};
       r[c.master_metadata_album_album_name].total_time += Math.trunc(c.ms_played/1000);
@@ -83,7 +102,38 @@ const mostListenedAlbum = () => {
       return r;
     },{}));
 
-  list.value.sort((a: any, b: any) => b.count - a.count);
+  list.sort((a: any, b: any) => b.count - a.count);
+  return list;
+}
+
+const mostListenedHour = ():any[] => {
+  // regroupement par heure
+  const list = Object.values(dataParsed.value.reduce(
+    (r: any, c: Song) => {
+      const hour = dayjs(c.ts).hour();
+      r[hour] = r[hour] || {hour, total_time: Math.trunc(c.ms_played/1000), count: 1};
+      r[hour].total_time += Math.trunc(c.ms_played/1000);
+      r[hour].count += 1;
+      return r;
+    },{}));
+
+  list.sort((a: any, b: any) => b.count - a.count);
+  return list;
+}
+
+const mostListenedDay = ():any[] => {
+  // regroupement par heure
+  const list = Object.values(dataParsed.value.reduce(
+    (r: any, c: Song) => {
+      const day = dayjs(c.ts).day();
+      r[day] = r[day] || {day, total_time: Math.trunc(c.ms_played/1000), count: 1};
+      r[day].total_time += Math.trunc(c.ms_played/1000);
+      r[day].count += 1;
+      return r;
+    },{}));
+
+  list.sort((a: any, b: any) => b.count - a.count);
+  return list;
 }
 </script>
 
@@ -107,25 +157,37 @@ const mostListenedAlbum = () => {
       v-model="tab"
       bg-color="primary"
     >
-    <v-tab value="one" @click="mostListenedSong">Most listened-to song</v-tab>
-    <v-tab value="two" @click="mostListenedArtist">Most listened-to artist</v-tab>
-    <v-tab value="three" @click="mostListenedAlbum">Most listened-to album</v-tab>
+    <v-tab value="1" @click="mostListenedSong">Most listened-to song</v-tab>
+    <v-tab value="2" @click="mostListenedArtist">Most listened-to artist</v-tab>
+    <v-tab value="3">Most listened-to album</v-tab>
+    <v-tab value="4">Most listened-to hour</v-tab>
+    <v-tab value="5">Most listened-to day</v-tab>
   </v-tabs>
   <v-card-text>
       <v-tabs-window v-model="tab">
-        <v-tabs-window-item value="one">
-          <div v-if="list.length > 0" v-for="(song, index) in list">
-            <div>n°{{index+1}} : {{ song.name }} by {{ song.artist }} - {{ song.count }} listens - {{ Math.trunc(song.total_time/(3600*24)) }}d{{ Math.trunc(song.total_time%(3600*24)/3600) }}h{{ Math.trunc((song.total_time%3600)/60) }}m{{ (song.total_time%3600)%60 }}s</div>
+        <v-tabs-window-item value="1">
+          <div v-for="(song, index) in mostListenedSongList">
+            <div>n°{{index+1}} : {{ song.name }} by {{ song.artist }} - {{ song.count }} listens - {{ durationParsed(song.total_time) }}</div>
           </div>
         </v-tabs-window-item>
-        <v-tabs-window-item value="two">
-          <div v-if="list.length > 0" v-for="(artist, index) in list">
-            <div>n°{{index+1}} : {{ artist.artist }} - listened to {{ artist.count }} times for {{ Math.trunc(artist.total_time/(3600*24)) }}d{{ Math.trunc(artist.total_time%(3600*24)/3600) }}h{{ Math.trunc((artist.total_time%3600)/60) }}m{{ (artist.total_time%3600)%60 }}s</div>
+        <v-tabs-window-item value="2">
+          <div v-for="(artist, index) in mostListenedArtistList">
+            <div>n°{{index+1}} : {{ artist.artist }} - listened to {{ artist.count }} times for {{ durationParsed(artist.total_time) }}</div>
           </div>
         </v-tabs-window-item>
-        <v-tabs-window-item value="three">
-          <div v-if="list.length > 0" v-for="(album, index) in list">
-            <div>n°{{index+1}} : {{ album.album }} - listened {{ album.count }} songs of it for {{ Math.trunc(album.total_time/(3600*24)) }}d{{ Math.trunc(album.total_time%(3600*24)/3600) }}h{{ Math.trunc((album.total_time%3600)/60) }}m{{ (album.total_time%3600)%60 }}s</div>
+        <v-tabs-window-item value="3">
+          <div v-for="(album, index) in mostListenedAlbum()">
+            <div>n°{{index+1}} : {{ album.album }} - listened {{ album.count }} songs of it for {{ durationParsed(album.total_time) }}</div>
+          </div>
+        </v-tabs-window-item>
+        <v-tabs-window-item value="4">
+          <div v-for="(hour, index) in mostListenedHour()">
+            <div>n°{{index+1}} : {{ hour.hour }}h for {{ durationParsed(hour.total_time) }}</div>
+          </div>
+        </v-tabs-window-item>
+        <v-tabs-window-item value="5">
+          <div v-for="(day, index) in mostListenedDay()">
+            <div>n°{{index+1}} : {{ dayjs().weekday(day.day).format('dddd') }} for {{ durationParsed(day.total_time) }}</div>
           </div>
         </v-tabs-window-item>
       </v-tabs-window>
